afcc920357fbeddbcc7b75d2c5e69a53804af019
Joao Dias <jfcdias@sga.pucminas.br>
2018-03-20 11:06:47 -0300

    interface

--- Client.java
+++ Client.java
@@ -1,3 +1,4 @@
+
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 
@@ -9,28 +10,43 @@ import java.security.PublicKey;
 
 import javax.crypto.Cipher;
 
-public class Client {
+public class Client implements NewJFrame.RMISelectFileInterface {
+    private String host;
 
-	    private Client() {}
+    public Client(String host) {
+        this.host = host;
+    }
 
-		public static void main(String[] args) {
+    public void waitForFile() {
+        NewJFrame jframe = new NewJFrame(this);
+        jframe.pack();
+        jframe.setLocationRelativeTo(null);
+        jframe.setVisible(true);
+    }
 
-			String host = (args.length < 1) ? null : args[0];
-			try {
-				Registry registry = LocateRegistry.getRegistry(host);
-				Hello stub = (Hello) registry.lookup("Hello");
-				
-				Cripto c = new Cripto();
-				KeyPair keys = c.buildKeyPair();
+    @Override
+    public void fileToString(String file) {
+        try {
+            Registry registry = LocateRegistry.getRegistry(host);
+            Hello stub = (Hello) registry.lookup("Hello");
 
-				byte[] encrypted = c.encrypt(keys.getPrivate(),c.sha512("teste").getBytes());
-				Send send = new Send(keys.getPublic().getEncoded(),encrypted,"teste");
-				
-				String response = stub.sayHello( send );
-				System.out.println("response: " + response);
-			} catch (Exception e) {
-				System.err.println("Client exception: " + e.toString());
-				e.printStackTrace();
-		 	}
-		}
+            Cripto c = new Cripto();
+            KeyPair keys = c.buildKeyPair();
+
+            byte[] encrypted = c.encrypt(keys.getPrivate(), c.sha512(file).getBytes());
+            Send send = new Send(keys.getPublic().getEncoded(), encrypted, file);
+
+            String response = stub.sayHello(send);
+            System.out.println("response: " + response);
+        } catch (Exception e) {
+            System.err.println("Client exception: " + e.toString());
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        String host = (args.length < 1) ? null : args[0];
+        Client client = new Client(host);
+        client.waitForFile();
+    }
 }
